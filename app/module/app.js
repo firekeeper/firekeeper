eed9e1d07a745d97a2cd6e04b9708540a2a2ba02
刘家尧 <x@kassading.com>
2016-11-09 16:15:46 +0800

    修复 Async 的注册问题

--- app/module/app.js
+++ app/module/app.js
@@ -1,17 +1,1 @@
 import './common/style.sass'
-
-import Vue from 'vue'
-
-const vm = new Vue({
-    el: '#app-8',
-    data: {
-        a: 1
-    },
-    computed: {
-        b: function() {
-            return this.a + 1
-        }
-    }
-})
-
-window.vm = vm
